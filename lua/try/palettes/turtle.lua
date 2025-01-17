639d485b37122372ee8fd064723f9e8784076810
UnaTried <103455203+UnaTried@users.noreply.github.com>
2025-01-17 20:06:30 +0000

    I have to use my text color

--- lua/try/palettes/turtle.lua
+++ lua/try/palettes/turtle.lua
@@ -22,9 +22,9 @@ return {
 	overlay2 = "#777777",
 	overlay1 = "#7bc427",
 	overlay0 = "#c8daf4",
-	surface2 = "#7bc427",
-	surface1 = "#c8daf4", -- not done
-	surface0 = "#0F1728", -- not done
+	surface2 = "#0F1728",
+	surface1 = "#7c94e9", -- not done
+	surface0 = "#0f1728", -- not done
 	base = "#09111d",
 	mantle = "#0f1728",
 	crust = "#0f1728",
