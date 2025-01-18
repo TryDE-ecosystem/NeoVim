d91b1baedbbd1a7a5a88d0b78382bed01bf81fa2
UnaTried <103455203+UnaTried@users.noreply.github.com>
2025-01-18 13:29:20 +0000

    Maybe collapsed code and the seperator is the same :sob:

--- lua/try/palettes/turtle.lua
+++ lua/try/palettes/turtle.lua
@@ -21,7 +21,7 @@ return {
 	subtext0 = "#c8daf4",
 	overlay2 = "#777777",
 	overlay1 = "#7bc427",
-	overlay0 = "#7bc427", -- Collapsed code
+	overlay0 = "#0f1728", -- Collapsed code
 	surface2 = "#7bc427",
 	surface1 = "#7bc427", -- Neovim seperator/folder indicator
 	surface0 = "#0f1728",
